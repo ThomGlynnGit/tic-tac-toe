ac00e3eb5d30034eef348dbe39b27b48b76f3959
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-02-10 17:28:22 +0000

    create win controller

--- script.js
+++ script.js
@@ -25,11 +25,12 @@ function createPlayer(name, marker) {
 function createGame(gameBoard, p1Name, p2Name){
     const player1 = createPlayer(p1Name, "O")
     const player2 = createPlayer(p2Name, "X")
+    let complete = false 
 
-    return { gameBoard, player1, player2 }
+    return { gameBoard, player1, player2, complete }
 }
 
-function turnController(game) {
+function createTurnController(game) {
     let turn = game.player1
     function changeTurn(player){
         if (player === game.player1){
@@ -54,11 +55,37 @@ function turnController(game) {
     }
 }
 
+function completeController(game) {
+    if(game.gameBoard[0][0] === game.gameBoard[0][1] && game.gameBoard[0][0] === game.gameBoard[0][2]){
+        game.complete = true
+    }
+    else if(game.gameBoard[1][0] === game.gameBoard[1][1] && game.gameBoard[1][0] === game.gameBoard[1][2]){
+        game.complete = true
+    }
+    else if(game.gameBoard[2][0] === game.gameBoard[2][1] && game.gameBoard[2][0] === game.gameBoard[2][2]){
+        game.complete = true
+    }
+    else if(game.gameBoard[0][0] === game.gameBoard[1][0] && game.gameBoard[0][0] === game.gameBoard[2][0]){
+        game.complete = true
+    }
+    else if(game.gameBoard[0][1] === game.gameBoard[1][1] && game.gameBoard[0][1] === game.gameBoard[2][1]){
+        game.complete = true
+    }
+    else if(game.gameBoard[0][2] === game.gameBoard[1][2] && game.gameBoard[0][2] === game.gameBoard[2][2]){
+        game.complete = true
+    }
+    else if(game.gameBoard[0][0] === game.gameBoard[1][1] && game.gameBoard[0][0] === game.gameBoard[2][2]){
+        game.complete = true
+    }
+    else if(game.gameBoard[0][2] === game.gameBoard[1][1] && game.gameBoard[0][2] === game.gameBoard[2][0]){
+        game.complete = true
+    }  
+}
 
 
 
 const newGame = createGame(createBoard, "Ben", "Test")
-const newGameFlow = turnController(newGame)
+const newGameFlow = createTurnController(newGame)
 
 console.log(newGameFlow.currentPlayer())
 newGameFlow.player2Turn()
