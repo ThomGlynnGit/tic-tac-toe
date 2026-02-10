eecd0238dd06a847c3470aa3fc891d026fb65855
Thomas Taylor <98777656+ThomGlynnGit@users.noreply.github.com>
2026-02-10 11:27:57 +0000

    create display game board function
    - change empty strings in array to underscore

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const gameBoard = (() => {
     let gameArr = []
     for(let i = 0; i < 3; i++){
-        gameArr.push(["", "", ""])
+        gameArr.push(["_", "_", "_"])
     }
 
     return gameArr
 })()
 
+function displayBoard (board) {
+    let row = ""
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < board.length; j++){
+            row += (board[i][j] + " ")
+        }
+        console.log((i+1) + ": " + row)
+        row = ""
+    }
+}
+
 function createPlayer(name, marker) {
     return {name, marker}
 }
 
-
-
-console.log(gameBoard)
+displayBoard(gameBoard)
